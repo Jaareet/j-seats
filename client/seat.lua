5bf1a441c55ba58ca09e0d56d4e84704f69a0c99
Jaareet <jaareet.soporte@gmail.com>
2023-01-25 21:13:50 +0000

    feat(main) Added files

---
+++ client/seat.lua
@@ -0,0 +1,60 @@
+local SEATS = {
+    [1] = {
+        label = 'del conductor',
+        val = -1,
+    },
+    [2] = {
+        label = 'del copiloto',
+        val = 0,
+    },
+    [3] = {
+        label = 'detras del conductor',
+        val = 1,
+    },
+    [4] = {
+        label = 'detras del copiloto',
+        val = 2,
+    }
+}
+local lastPlayerSeat;
+local function ShowNotification(text, type, timeouts)
+    local event = { 'esx:showNotification', text, type or '', timeouts or 7000 }
+    TriggerEvent(table.unpack(event))
+end
+RegisterCommand('seat', function(src, args)
+    if (IsPedInAnyVehicle(PlayerPedId())) then
+        if (tonumber(args[1])) then
+            if (SEATS[tonumber(args[1])]) then 
+                local playerPed = PlayerPedId()
+                local vehicle = GetVehiclePedIsIn(playerPed, false)
+                local seat = SEATS[tonumber(args[1])].val
+                local getPlayerCurrentSeat = GetPedInVehicleSeat(vehicle, seat)
+                if (lastPlayerSeat ~= SEATS[tonumber(args[1])].val) then 
+                    lastPlayerSeat = SEATS[tonumber(args[1])].val
+                    print(lastPlayerSeat, seat, getPlayerCurrentSeat)
+                    ShowNotification(('Te has cambiado al asiento %s'):format(SEATS[tonumber(args[1])].label), 'success')
+                    if (getPlayerCurrentSeat ~= seat) or (getPlayerCurrentSeat == 0) then  -- Esto porque el GTA va un poco de lado la verdad...
+                        SetPedIntoVehicle(playerPed, vehicle, seat)
+                    else
+                        ShowNotification('Ese asiento ya esta ocupado', 'error')
+                    end
+                else
+                    ShowNotification(('Ya estas en el asiento %s'):format(SEATS[tonumber(args[1])].label), 'error')
+                end
+            else
+                ShowNotification('No existe ese asiento', 'error')
+            end
+        else
+            ShowNotification('Debes de poner un asiento', 'error')
+        end
+    end
+end)
+
+RegisterNetEvent('onResourceStart', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+        return
+    end
+    if (IsDuplicityVersion()) then 
+        print(('^2[WARN]^0 %s'):format('Debes de poner el codigo en client side, no es apto para serverside.'))
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Jaareet <@Jaareet#0542>'
+description 'Seat Changer'
+version '1.0.0'
+lua54 'yes'
+
+-- What to run
+client_scripts {
+    'client/s*.lua'
+}
